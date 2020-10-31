e55feee1cc7b6d517d62ed948e43da2f3fa48cb0
Taha Firoz <i170323@nu.edu.pk>
2020-10-31 07:18:05 +0500

    added loading, password field and bug fixes

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -4,60 +4,107 @@
   import { onDestroy } from "svelte";
   metatags.title = "CloudSafe Editor";
   metatags.description = "The Cloudsafe Policy Editor";
-  import { Button, Modal, DataTable, Pagination } from "carbon-components-svelte";
-  import { json_policy_data } from '../stores.js';
+  import {
+    Button,
+    Modal,
+    DataTable,
+    Pagination,
+    InlineLoading,
+  } from "carbon-components-svelte";
+  import { json_policy_data } from "../stores.js";
+  import { axiosAPI, wait } from "../init_axios";
 
   let open = false;
+  let open_weirdo = false;
 
-  const headers = [];
+  let heading = "";
 
-  const all_rows = [];
+  let headers = [];
 
-  let paginated_rows =  []
+  let all_rows = [];
 
-  const response = [
+  let paginated_rows = [];
+
+  let fetching = null;
+
+  let response = [
     {
       title: "Conflicts",
       value: "?",
-      meta: [],
+      meta: {},
     },
     {
       title: "Redundancies",
       value: "?",
-      meta: [],
+      meta: {},
     },
     {
       title: "Users",
       value: "?",
-      meta: [],
+      meta: {},
     },
     {
       title: "Groups",
       value: "?",
-      meta: [],
+      meta: {},
     },
   ];
 
   let ace_editor = null;
 
-  const unsubscribe = json_policy_data.subscribe((value)=>{
-      if(ace_editor){
-        ace_editor.set(value);
-      }
-  })
+  const unsubscribe = json_policy_data.subscribe((value) => {
+    if (ace_editor) {
+      ace_editor.set(value);
+    }
+  });
 
-  function attach(e) {
+  function initJsonEditor(e) {
     const editor = new jsoneditor(e, {
       mode: "code",
       theme: "ace/theme/twilight",
       mainMenuBar: false,
     });
     ace_editor = editor;
-    editor.set(["Import your policy with your credentials","or","Paste your policy here"]);
+    editor.set([
+      "Import your policy with your credentials",
+      "or",
+      "Paste your policy here",
+    ]);
   }
 
-  function paginate({page, pageSize}){
-    paginated_rows = all_rows.slice((page-1)*pageSize, page*pageSize)
+  function paginate({ page, pageSize }) {
+    paginated_rows = all_rows.slice((page - 1) * pageSize, page * pageSize);
+  }
+
+  function checkPolicy() {
+    try {
+      const parsed = JSON.parse(ace_editor.getText());
+
+      if (Array.isArray(parsed)) {
+        return;
+      } else {
+        fetching = "active";
+        axiosAPI
+          .post("/anomalies/conflicts", JSON.parse(ace_editor.getText()))
+          .then((res) => {
+            wait(2000).then(()=>{fetching = "finished";response = res.data;
+            response.splice(1, 0, {
+              title: "Redundancies",
+              value: "?",
+              meta: {},
+            });wait(2000).then(() => (fetching = null));})
+            
+            
+          })
+          .catch((e) => {
+            wait(2000).then(()=>{fetching = "error";wait(2000).then(() => (fetching = null));})
+            console.log(e);
+          });
+      }
+    } catch (e) {
+      console.log(e);
+      return;
+    }
   }
   onDestroy(unsubscribe);
 </script>
@@ -73,49 +120,87 @@
 </svelte:head>
 
 <div class="h-full w-full flex" theme="g90">
-  <div class="flex flex-col w-1/3 h-full border-r-2 border-solid border-carbon-90">
+  <div
+    class="flex flex-col w-1/3 h-full border-r-2 border-solid border-carbon-90">
     <div class="flex bg-carbon-80 p-2 justify-between items-center">
-        <span class="font-bold ml-4 inline-flex align-middle">
-            AWS IAM Policies (JSON)
-        </span>
-        <Button size="small">Update</Button>
+      <span class="font-bold ml-4 inline-flex align-middle">
+        AWS IAM Policies (JSON)
+      </span>
+      <div class="w-40">
+        {#if fetching === null}
+          <Button on:click={checkPolicy} size="small">Update</Button>
+        {:else if fetching === 'active'}
+          <InlineLoading status="active" description="Fetching Policy" />
+        {:else if fetching === 'finished'}
+          <InlineLoading status="finished" description="Fetch Successful" />
+        {:else if fetching === 'error'}
+          <InlineLoading status="error" description="Failed to Fetch" />
+        {/if}
+      </div>
     </div>
-    <div id="jsoneditor" use:attach class="flex-1" />
+    <div id="jsoneditor" use:initJsonEditor class="flex-1" />
   </div>
   <div class="grid w-2/3 bg-carbon-100 place-items-center">
     <div class="grid grid-cols-2 gap-10 ">
       {#each response as item, i}
         <div
-          class="group w-48 h-32 bg-white p-4 {item.title === "Redundancies"? "": "cursor-pointer hover:shadow-2xl hover:bg-cablue-60"}">
+          class="group w-48 h-32 bg-white p-4 {item.title === 'Redundancies' || item.value === '?' ? 'bg-gray-400 cursor-not-allowed' : 'cursor-pointer hover:shadow-2xl hover:bg-cablue-60'}">
           <div
             class="flex flex-col justify-between h-full"
             on:click={() => {
-                if(item.title !== "Redundancies"){
-                    open = true;
-                    paginate({page: 1, pageSize: 10})
-                }
+              if (item.title !== 'Redundancies' || item.value === '?') {
+                item.title === 'Conflicts'? open_weirdo = true: open = true;
+                heading = item.title;
+                headers = item.meta.headers;
+                all_rows = item.meta.values;
+                paginate({ page: 1, pageSize: 10 });
+              }
             }}>
-            <span class="font-bold text-carbon-100 {item.title === "Redundancies"? "": "group-hover:text-white"} text-lg">{item.title}</span>
-            <span class="font-bold text-carbon-100 {item.title === "Redundancies"? "": "group-hover:text-white"} text-5xl text-right">{item.value}</span>
+            <span
+              class="font-bold text-carbon-100 {item.title === 'Redundancies' || item.value === '?' ? '' : 'group-hover:text-white'} text-lg">{item.title}</span>
+            <span
+              class="font-bold text-carbon-100 {item.title === 'Redundancies' || item.value === '?' ? '' : 'group-hover:text-white'} text-5xl text-right">{item.value}</span>
           </div>
         </div>
       {/each}
     </div>
 
     <Modal
+        bind:open={open_weirdo}
+        modalHeading={heading}
+        passiveModal
+        hasForm
+        hasScrollingContent>
+      <DataTable expandable {headers} rows={paginated_rows}>
+        <div slot="expanded-row" let:row>
+          <pre>
+                {JSON.stringify(row, null, 2)}
+                </pre>
+        </div>
+      </DataTable>
+      <Pagination
+        on:update={(e) => {
+          e = e.detail;
+          paginate(e);
+        }}
+        pageSizeInputDisabled
+        totalItems={all_rows.length} />
+    </Modal>
+
+    <Modal
       bind:open
-      modalHeading="Create database"
-      primaryButtonDisabled
-      secondaryButtonText="Back"
-      on:click:button--secondary={() => (open = false)}
-      on:open
-      on:close
-      on:submit={() => (open = false)}
-      >
-        <DataTable
-        headers={headers}
-        rows={paginated_rows} />
-        <Pagination on:update={(e)=>{e = e.detail; paginate(e)}} pageSizeInputDisabled totalItems={all_rows.length} />
+      modalHeading={heading}
+      passiveModal
+      hasForm
+      hasScrollingContent>
+      <DataTable {headers} rows={paginated_rows} />
+      <Pagination
+        on:update={(e) => {
+          e = e.detail;
+          paginate(e);
+        }}
+        pageSizeInputDisabled
+        totalItems={all_rows.length} />
     </Modal>
   </div>
 </div>
